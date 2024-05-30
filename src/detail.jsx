d3721bd1e9b1b190a4646eb6024758d54eb06c60
Sufyan Ali <160006127+Sufyan154@users.noreply.github.com>
2024-05-30 16:16:02 +0500

    Add files via upload

---
+++ src/detail.jsx
@@ -0,0 +1,38 @@
+export const Data = [
+    {
+        id:12,
+        firstName:"Danish",
+        lastName:"Shahzad",
+        age:19
+
+    },
+    {
+        id:13,
+        firstName:"Ali",
+        lastName:"Raza",
+        age:20
+
+    },
+    {
+        id:13,
+        firstName:"Sahil",
+        lastName:"meer",
+        age:24
+
+    },
+    {
+        id:13,
+        firstName:"Sufyan",
+        lastName:"Ali",
+        age:20
+
+    },
+    {
+        id:14,
+        firstName:"Muhib",
+        lastName:"Rehman", 
+        age:19
+
+    }
+
+]
